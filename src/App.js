775a0c81b13daf375acc79538621dfce518e8db6
Hernan Albertario <hernandres64@hotmail.com>
2018-01-03 20:24:22 -0300

    Move confiureStore to a new file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import ReduxThunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
-import logger from 'redux-logger'
 import { StatusBar, View, Platform } from 'react-native';
 import Router from './Router';
-
-const store = createStore(reducers, {}, applyMiddleware(ReduxThunk, logger));
+import configureStore from './Store';
 
 Number.prototype.thousandDot = function(){
     return this.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
@@ -19,7 +14,7 @@ const Style = {
 }
 
 const App = () => (
-    <Provider store={store}>
+    <Provider store={configureStore}>
         <View style={Style}>
             <StatusBar backgroundColor={'black'} />
             <Router />
