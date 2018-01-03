ce5cd93dfe132f4611d234c5cc940822bc1ea0bc
Hernan <hernandres64@hotmail.com>
2018-01-03 14:54:58 -0300

    Generic fetch method added

--- src/actions/HotelActions.js
+++ src/actions/HotelActions.js
@@ -1,5 +1,6 @@
 import { Actions } from 'react-native-router-flux';
 import api from './../helpers/api'
+import { genericListFetch } from './UtilActions'
 
 import {
     HOTEL_LIST_FETCH,
@@ -13,66 +14,57 @@ import {
 } from './types';
 
 export const hotelListFetch = () => {
-
     return (dispatch) => {
-
-        dispatch({ type: HOTEL_LIST_FETCH });
-
-        var url = api.getHotelListlUrl();
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
-        })
-            .then((response) => {
-                return response.json()
-            })
-            .then((rjson) => {
-                if (rjson !== null) {
-                    dispatch({ type: HOTEL_LIST_FETCH_SUCCESS, payload: { list: rjson.hotels } });
-                }
-                else {
-                    dispatch({ type: HOTEL_LIST_FETCH_SUCCESS, payload: { list: [] } });
-                }
-            })
-            .catch((error) => {
-                console.error(error)
-                dispatch({ type: HOTEL_LIST_FETCH_FAIL, payload: { error: 'Ha ocurrido un error al cargar los hoteles.' } })
-            })
+        return dispatch(
+            genericListFetch(
+                HOTEL_LIST_FETCH,
+                api.getHotelListlUrl(),
+                loadHotelListSuccess,
+                loadHotelListFail,
+                ' Los hoteles.'
+            ))
     }
 };
 
+const loadHotelListSuccess = (dispatch, jsonResponse) => {
+    dispatch({
+        type: HOTEL_LIST_FETCH_SUCCESS,
+        payload:  {list: jsonResponse.hotels }
+    });
+};
+
+const loadHotelListFail = (dispatch, error) => {
+    dispatch({
+        type: HOTEL_LIST_FETCH_FAIL,
+        payload: { list: [], error }
+    });
+};
+
 export const hotelFetch = (id) => {
-    return (dispatch, getState) => {
-
-        dispatch({ type: HOTEL_FETCH });
-
-        var url = api.getHotelListlUrl() + id;
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
-        })
-            .then((response) => {
-                return response.json()
-            })
-            .then((rjson) => {
-                if (rjson !== null) {
-                    dispatch({ type: HOTEL_FETCH_SUCCESS, payload: { selectedHotel: rjson.hotel } });
-                }
-                else {
-                    dispatch({ type: HOTEL_FETCH_SUCCESS, payload: { selectedHotel: {} } });
-                }
-            })
-            .catch((error) => {
-                console.error(error)
-                dispatch({ type: HOTEL_FETCH_FAIL, payload: { error: 'Ha ocurrido un error al cargar los hoteles.' } })
-            })
+    return (dispatch) => {
+        return dispatch(
+            genericListFetch(
+                HOTEL_FETCH,
+                api.getHotelListlUrl() + id,
+                loadHotelSuccess,
+                loadHotelFail,
+                ' El hotel.'
+            ))
     }
+};
+
+const loadHotelSuccess = (dispatch, rjson) => {
+    dispatch({
+        type: HOTEL_FETCH_SUCCESS,
+        payload:  {selectedHotel: rjson.hotel }
+    });
+};
+
+const loadHotelFail = (dispatch, error) => {
+    dispatch({
+        type: HOTEL_FETCH_FAIL,
+        payload: { selectedHotel: {}, error }
+    });
 };
 
 export const filterChanged = ({ text }) => {
